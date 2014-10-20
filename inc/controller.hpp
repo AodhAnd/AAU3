2369f76ad6855ed3e4427f04b7434d6334215baf
Frederik Juul <Frederikj@gmail.com>
2014-10-20 23:23:44 +0200

    Updated controller structure with comments about how to move forward

--- inc/controller.hpp
+++ inc/controller.hpp
@@ -7,20 +7,42 @@
 
 #ifndef CONTROLLER_HPP_
 #define CONTROLLER_HPP_
+#include <imu.hpp>
 
 class controller {
-/*
- * Everything that is needed to interface with the python controller.
- * The constructor could specify whether a python script is used or one
- * wants to implement the controller in cpp
- */
+
+	/* Class should inherit from Thread.
+	 *
+	 * New controllers should inherit this class and define the control loop.
+	 *class PIcontroller : public controller {
+	 *	void loop(){
+	 *		bla bla bla
+	 *	}
+	 *}
+	 *
+	 *The below is a suggestion for behavior.
+	 *
+	 *
+	 */
 public:
 	controller(controller&);
-	controller(Imu* pImu1, Imu* pImu2, MomentumMotor* pMotor, bool pythonImpl);
+	controller(Imu* pImu[], MomentumMotor* pMotor[]);
 	~controller();
+	void setPeriod(int time); // set period time in us.
+	int getPeriod();
+	virtual void loop(); // Define control loop. May contain awesome controller as default.
+	int start(); //Start Control loop. Returns 0 if no error?
+	void stop(); //Stop Control loop.
+
 
 private:
-	bool isPython;
+	int _time;
+	Imu *_pImus[];
+	MomentumMotor *_pMotors[];
+
+	int getImu(int imu); // Potentially split into different variables instead with a behind the scenes update function to guarantee that all IMUs are sampled at the same time?
+	int setMotor(int motor, int speed); //Same thing, should another function be called which actually writes motor values at the same time, at the end of the loop function?
+
 };
 
 
