08030b38f401fb96ddc914ec31790f646f6f3c82
Frederik Juul <Frederikj@gmail.com>
2014-11-17 20:32:47 +0100

    Added script for enabling pwm

---
+++ pwm_script.sh
@@ -0,0 +1,4 @@
+echo am33xx_pwm > /sys/devices/bone_capemgr.9/slots
+echo bone_pwm_P8_13 > /sys/devices/bone_capemgr.9/slots
+echo bone_pwm_P9_14 > /sys/devices/bone_capemgr.9/slots
+echo bone_pwm_P9_16 > /sys/devices/bone_capemgr.9/slots
